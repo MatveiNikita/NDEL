63dffb0615d8a750bf982111e9822b4a39451558
nikita-matvei <matveinikitaivanovich@gmail.com>
2025-07-15 12:52:49 +0000

    Update .gitignore file

--- auth-service/src/main/java/nikdev/ndel/authservice/models/User.java
+++ auth-service/src/main/java/nikdev/ndel/authservice/models/User.java
@@ -1,10 +1,13 @@
 package nikdev.ndel.authservice.models;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import nikdev.ndel.authservice.models.roles.Role;
+import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDateTime;
 import java.util.Date;
@@ -20,13 +23,26 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
+
     @Enumerated(EnumType.STRING)
     private Role role;
+
+    @NotBlank
     private String name;
+
+    @NotBlank
     private String surname;
+
+    @Email
+    @Column(nullable = false, unique = true) //nullable - е допускает появления null в БД, а @NotNull в java программе
     private String email;
+
+    @Column(nullable = false)
     private String password;
+    
     private String phoneNumber;
+
     private Date dateOfBirth;
+    @CreationTimestamp
     private LocalDateTime createdTime;
 }
